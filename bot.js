7ea15a8f4612fcfb1880749679a9387b1d1eeb9d
Alexander Bergholm <bergholm.alexander@gmail.com>
2020-09-19 17:04:26 +0300

    added comments to the first two functions

--- bot.js
+++ bot.js
@@ -1,101 +1,104 @@
-import Discord from "discord.js"
-import config from "./config.json"
-import {getPlayerStats, getPlayerStats20} from "./faceIt.js"
-const client = new Discord.Client()
+import Discord from "discord.js";
+import config from "./config.json";
+import { getPlayerStats, getPlayerStats20 } from "./faceIt.js";
+const client = new Discord.Client();
 
-const winEmoji = ":green_square:"
-const lossEmoji = ":red_square:"
+const winEmoji = ":green_square:";
+const lossEmoji = ":red_square:";
 
 client.on("ready", () => {
-    console.log("I am ready!")
-})
+  console.log("I am ready!");
+});
 
 client.on("message", async (message) => {
-    if (!message.content.startsWith(config.prefix) || message.author.bot) return
-    const args = message.content.slice(config.prefix.length).trim().split(/ +/g)
-    const command = args.shift().toLowerCase()
-    if (command === 'stats') {
-        const player = args[0]
-        const stats = await getPlayerStats(player)
-        const embed = {
-            "title": player+"'s Lifetime FACEIT Stats",
-            "url": "https://www.faceit.com/en/players/"+player,
-            "color": 12345678,
-            "thumbnail": {
-              "url": stats.avatar
-            },
-            "author": {
-              "name": player,
-              "url": "https://www.faceit.com/en/players/"+player,
-              "icon_url": stats.skill_img
-            },
-            "fields": [
-              {
-                "name": "Recent Results",
-                "value": stats.recent_results.map(a => a==='1' ? winEmoji : lossEmoji).join(" , ")
-              },
-              {
-                "name": "Elo",
-                "value": stats.elo
-              },
-              {
-                "name": "K/D Ratio",
-                "value": stats.kdr
-              },
-              {
-                "name": "Average HS%",
-                "value": stats.average_hs
-              },
-              {
-                "name": "Best Map",
-                "value": "Winrate "+stats.best_map["mapWR"]+"% on map "+stats.best_map["map"]
-              }
-            ]
-          };
-          message.channel.send({ embed });
-    } else if (command === 'stats20') {
-      const player = args[0]
-      const stats = await getPlayerStats(player)
-      const stats20 = await getPlayerStats20(stats["player_id"])
-      const embed = {
-        "title": player+"'s 20-game average FACEIT stats",
-        "url": "https://www.faceit.com/en/players/"+player,
-        "color": 12345678,
-        "thumbnail": {
-          "url": stats.avatar
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if (command === "stats") {
+    const player = args[0];
+    const stats = await getPlayerStats(player);
+    const embed = {
+      title: player + "'s Lifetime FACEIT Stats",
+      url: "https://www.faceit.com/en/players/" + player,
+      color: 12345678,
+      thumbnail: {
+        url: stats.avatar,
+      },
+      author: {
+        name: player,
+        url: "https://www.faceit.com/en/players/" + player,
+        icon_url: stats.skill_img,
+      },
+      fields: [
+        {
+          name: "Recent Results",
+          value: stats.recent_results
+            .map((a) => (a === "1" ? winEmoji : lossEmoji))
+            .join(" , "),
         },
-        "author": {
-          "name": player,
-          "url": "https://www.faceit.com/en/players/"+player,
-          "icon_url": stats.skill_img
+        {
+          name: "Elo",
+          value: stats.elo,
         },
-        "fields": [
-          {
-            "name": "Average Kills",
-            "value": Math.ceil(stats20.Kills)
-          },
-          {
-            "name": "Average Headshots %",
-            "value": stats20["Headshots %"]
-          },
-          {
-            "name": "Average K/D",
-            "value": stats20["K/D Ratio"].toFixed(2)
-          },
-          {
-            "name": "Average K/R",
-            "value": stats20["K/R Ratio"].toFixed(2)
-          }
-        ]
-      };
-      // TODO: Would be interesting to get best map in last 20 matches as well
-      message.channel.send({ embed });
-    } else if (command === 'match') {
-        const result = FaceIt.matchHandler(args[0])
-    }
-})
+        {
+          name: "K/D Ratio",
+          value: stats.kdr,
+        },
+        {
+          name: "Average HS%",
+          value: stats.average_hs,
+        },
+        {
+          name: "Best Map",
+          value:
+            "Winrate " +
+            stats.best_map["mapWR"] +
+            "% on map " +
+            stats.best_map["map"],
+        },
+      ],
+    };
+    message.channel.send({ embed });
+  } else if (command === "stats20") {
+    const player = args[0];
+    const stats = await getPlayerStats(player);
+    const stats20 = await getPlayerStats20(stats["player_id"]);
+    const embed = {
+      title: player + "'s 20-game average FACEIT stats",
+      url: "https://www.faceit.com/en/players/" + player,
+      color: 12345678,
+      thumbnail: {
+        url: stats.avatar,
+      },
+      author: {
+        name: player,
+        url: "https://www.faceit.com/en/players/" + player,
+        icon_url: stats.skill_img,
+      },
+      fields: [
+        {
+          name: "Average Kills",
+          value: Math.ceil(stats20.Kills),
+        },
+        {
+          name: "Average Headshots %",
+          value: stats20["Headshots %"],
+        },
+        {
+          name: "Average K/D",
+          value: stats20["K/D Ratio"].toFixed(2),
+        },
+        {
+          name: "Average K/R",
+          value: stats20["K/R Ratio"].toFixed(2),
+        },
+      ],
+    };
+    // TODO: Would be interesting to get best map in last 20 matches as well
+    message.channel.send({ embed });
+  } else if (command === "match") {
+    const result = FaceIt.matchHandler(args[0]);
+  }
+});
 
-
-
-
-client.login(config.token)
+client.login(config.token);
--- faceIt.js
+++ faceIt.js
@@ -1,126 +1,142 @@
-import axios from "axios"
-import config from "./config.json"
-axios.defaults.headers.common = {'Authorization': `bearer ${config.fci}`}
+import axios from "axios";
+import config from "./config.json";
+axios.defaults.headers.common = { Authorization: `bearer ${config.fci}` };
 
 const fiLevelUrls = {
-    "1": "https://support.faceit.com/hc/en-us/article_attachments/204362969/ss__2016-05-18_at_12.29.32_.jpg",
-    "2": "https://support.faceit.com/hc/en-us/article_attachments/204399625/ss__2016-05-18_at_06.42.10_.png",
-    "3": "https://support.faceit.com/hc/en-us/article_attachments/204362989/ss__2016-05-18_at_06.23.36_.png",
-    "4": "https://support.faceit.com/hc/en-us/article_attachments/204399525/ss__2016-05-18_at_06.23.58_.png",
-    "5": "https://support.faceit.com/hc/en-us/article_attachments/204399545/ss__2016-05-18_at_06.24.15_.png",
-    "6": "https://support.faceit.com/hc/en-us/article_attachments/204399565/ss__2016-05-18_at_06.24.39_.png",
-    "7": "https://support.faceit.com/hc/en-us/article_attachments/204363029/ss__2016-05-18_at_06.24.53_.png",
-    "8": "https://support.faceit.com/hc/en-us/article_attachments/204399585/ss__2016-05-18_at_06.25.06_.png",
-    "9": "https://support.faceit.com/hc/en-us/article_attachments/204399605/ss__2016-05-18_at_06.25.21_.png",
-    "10": "https://support.faceit.com/hc/en-us/article_attachments/204399485/ss__2016-05-18_at_12.30.17_.jpg"
-}
+  1: "https://support.faceit.com/hc/en-us/article_attachments/204362969/ss__2016-05-18_at_12.29.32_.jpg",
+  2: "https://support.faceit.com/hc/en-us/article_attachments/204399625/ss__2016-05-18_at_06.42.10_.png",
+  3: "https://support.faceit.com/hc/en-us/article_attachments/204362989/ss__2016-05-18_at_06.23.36_.png",
+  4: "https://support.faceit.com/hc/en-us/article_attachments/204399525/ss__2016-05-18_at_06.23.58_.png",
+  5: "https://support.faceit.com/hc/en-us/article_attachments/204399545/ss__2016-05-18_at_06.24.15_.png",
+  6: "https://support.faceit.com/hc/en-us/article_attachments/204399565/ss__2016-05-18_at_06.24.39_.png",
+  7: "https://support.faceit.com/hc/en-us/article_attachments/204363029/ss__2016-05-18_at_06.24.53_.png",
+  8: "https://support.faceit.com/hc/en-us/article_attachments/204399585/ss__2016-05-18_at_06.25.06_.png",
+  9: "https://support.faceit.com/hc/en-us/article_attachments/204399605/ss__2016-05-18_at_06.25.21_.png",
+  10: "https://support.faceit.com/hc/en-us/article_attachments/204399485/ss__2016-05-18_at_12.30.17_.jpg",
+};
 
 export async function getPlayerStats(playerName) {
-    try {
-        const result = await axios.get("https://open.faceit.com/data/v4/players?nickname="+playerName+"&game=csgo&game_player_id="+playerName)
-        const playerId = result.data.player_id
-        const elo = result.data.games.csgo.faceit_elo
-        const playerStats = await axios.get("https://open.faceit.com/data/v4/players/"+playerId+"/stats/csgo")
+  /*    .stats $player_name$ implementation. Gets player information from FACEIT Data API.
+        Input: playerName
+        Output: playerStats information (playerStats may change over time, see return)
+        Assumptions: This currently is made specifically for csgo. In the future, this could quickly be implemented for other games.
+  */
 
-        return {
-            "avatar": result.data.avatar,
-            "elo": elo,
-            "recent_results": playerStats.data.lifetime["Recent Results"],
-            "kdr": playerStats.data.lifetime["Average K/D Ratio"],
-            "skill_img": fiLevelUrls[result.data.games.csgo.skill_level],
-            "best_map": bestMapWinRateFinder(playerStats.data.segments),
-            "average_hs": playerStats.data.lifetime["Average Headshots %"],
-            "player_id": playerId
-               }
-    } catch (error) {
-        console.error(error)
-        return {}
-    }
+  try {
+    const result = await axios.get(
+      // get player_id so we can search for rest of the stats
+      "https://open.faceit.com/data/v4/players?nickname=" +
+        playerName +
+        "&game=csgo&game_player_id=" +
+        playerName
+    );
+    const playerId = result.data.player_id;
+    const elo = result.data.games.csgo.faceit_elo;
+    const playerStats = await axios.get(
+      // This get requires stats of a user to be retrieved via player_id rather than player_name
+      "https://open.faceit.com/data/v4/players/" + playerId + "/stats/csgo"
+    );
+    return {
+      // .stats information
+      avatar: result.data.avatar,
+      elo: elo,
+      recent_results: playerStats.data.lifetime["Recent Results"],
+      kdr: playerStats.data.lifetime["Average K/D Ratio"],
+      skill_img: fiLevelUrls[result.data.games.csgo.skill_level],
+      best_map: bestMapWinRateFinder(playerStats.data.segments),
+      average_hs: playerStats.data.lifetime["Average Headshots %"],
+      player_id: playerId,
+    };
+  } catch (error) {
+    console.error(error);
+    return {};
+  }
 }
 
-export async function getPlayerStats20(playerId){
-    // Average Kills
-    // Average Headshots
-    // Average KD
-    // Average K/R
-    // Winrates on maps for the last 20 games
+export async function getPlayerStats20(playerId) {
+  /*    .stats20 $player_name$ implementation. Gets player information from FACEIT Data API for the last 20 matches.
+        Input: playerId
+        Output: playerStats20 information (playerStats may change over time, see return)
+        Assumptions: This currently is made specifically for csgo. In the future, this could quickly be implemented for other games.
+  */
 
-    // Process
-    // Get last 20 maps and WR's
-    const twentyMatches = await getPastMatchesByPlayer(playerId)
-    const matchInfos = []
-    // Get match stat info & get stat info for specific player
+  const twentyMatches = await getPastMatchesByPlayer(playerId); // Gets winrates and match_id's for the last 20 games
+  const allPlayerMatches = [];
+  twentyMatches.matchIds.forEach(async (match) => {
+    allPlayerMatches.push(getMatchStats(match)); // Gets statistics of the match given match_id
+  });
 
-    // let statsPromises = []
-    // response.data.items.forEach(match => {
-    //     matchIds.push(match.match_id)
-    //     statsPromises.push(parseMatchWon(match, playerId))
-    // })
-
-    // const listOfResults = await Promise.all(statsPromises)
-
-    const allPlayerMatches = []
-    twentyMatches.matchIds.forEach( async (match) => {
-        allPlayerMatches.push(getMatchStats(match))
-    })
-
-    const twentyPlayerMatchStats = await Promise.all(allPlayerMatches)
-    let allPlayerMatchStats = []
-    twentyPlayerMatchStats.forEach( result => {
-        let teams = result["rounds"][0].teams
-        allPlayerMatchStats.push(teams[0]["players"].concat(teams[1]["players"]).find(pl => pl.player_id === playerId).player_stats)
-    })
-    // Sum together
-    let summedDict = {}
-    allPlayerMatchStats.forEach(matchPlayerStats => { // Always will be <= 20 matches
-        Object.keys(matchPlayerStats).forEach(pStatKey => {
-            if (!summedDict.hasOwnProperty(pStatKey)) {
-                summedDict[pStatKey] = Number(matchPlayerStats[pStatKey])
-            } else {
-                summedDict[pStatKey] += Number(matchPlayerStats[pStatKey])
-            }
-        })
-    })
-    Object.keys(summedDict).forEach(key => {
-        summedDict[key] = summedDict[key]/allPlayerMatchStats.length
-    })
-    console.log("summed dict")
-    console.log(summedDict)
-    return summedDict
+  const twentyPlayerMatchStats = await Promise.all(allPlayerMatches);
+  let allPlayerMatchStats = [];
+  twentyPlayerMatchStats.forEach((result) => {
+    // Gets the specific players stats for each match
+    let teams = result["rounds"][0].teams;
+    allPlayerMatchStats.push(
+      teams[0]["players"]
+        .concat(teams[1]["players"])
+        .find((pl) => pl.player_id === playerId).player_stats
+    );
+  });
+  let summedDict = {};
+  allPlayerMatchStats.forEach((matchPlayerStats) => {
+    // Sums together each stat for all games (i.e. total kills per match)
+    Object.keys(matchPlayerStats).forEach((pStatKey) => {
+      if (!summedDict.hasOwnProperty(pStatKey)) {
+        summedDict[pStatKey] = Number(matchPlayerStats[pStatKey]);
+      } else {
+        summedDict[pStatKey] += Number(matchPlayerStats[pStatKey]);
+      }
+    });
+  });
+  Object.keys(summedDict).forEach((key) => {
+    // Takes the average for each stat
+    summedDict[key] = summedDict[key] / allPlayerMatchStats.length;
+  });
+  return summedDict;
 }
 
 function bestMapWinRateFinder(mapData) {
-    let highest = ""
-    let highestWR = 0
-    mapData.forEach(mD => {
-        if (mD.stats["Win Rate %"] > highestWR && mD.stats["Matches"] > 20) {
-            highestWR = mD.stats["Win Rate %"]
-            highest = mD.label
-        }
-        })
-    return {"map": highest, "mapWR": highestWR}
+  /*    Looks for the highest winrate map of a player (for .stats) from "https://open.faceit.com/data/v4/players/" + {player_id} + "/stats/csgo" call
+        Input: JSON map data (data.segments)
+        Output: best map (string), winrate% (string)
+  */
+  // TODO: Currently hard defined 20 matches as min played for it to count in "best map winrate". Could be i.e. % of matches played?
+  let highest = "";
+  let highestWR = 0;
+  mapData.forEach((mD) => {
+    if (mD.stats["Win Rate %"] > highestWR && mD.stats["Matches"] > 20) {
+      highestWR = mD.stats["Win Rate %"];
+      highest = mD.label;
+    }
+  });
+  return { map: highest, mapWR: highestWR };
 }
 
-async function matchHandler(matchId) {
-    // Match ID Example 1-81792399-81a0-41f4-8cb3-26b789f684a5
-    const result = await axios.get("https://open.faceit.com/data/v4/matches/" + matchId)
-        .then(response => {
-            const teamOne = response.data.teams.faction1.roster
-            const teamTwo = response.data.teams.faction2.roster
-            // For each player, get json with winrates
-            getPastMatchesByPlayer("20dcc7de-c82b-4d12-9bbd-b9c448b63888").then((result) => {
-                console.log(result)
-            })
-            // Send info to discord
-        })
-        .catch(error => {
-            console.log(error)
-        })
-    return result
-}
+// WIP so is commented out as it is not used
+// async function matchHandler(matchId) {
+//   // Match ID Example 1-81792399-81a0-41f4-8cb3-26b789f684a5
+//   const result = await axios
+//     .get("https://open.faceit.com/data/v4/matches/" + matchId)
+//     .then((response) => {
+//       const teamOne = response.data.teams.faction1.roster;
+//       const teamTwo = response.data.teams.faction2.roster;
+//       // For each player, get json with winrates
+//       getPastMatchesByPlayer("20dcc7de-c82b-4d12-9bbd-b9c448b63888").then(
+//         (result) => {
+//           console.log(result);
+//         }
+//       );
+//       // Send info to discord
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+//   return result;
+// }
 
 async function getPastMatchesByPlayer(playerId, numMatches = 20) {
-    /* GET /players/{player_id}/history
+  /*  Retrieves the last 20 matches of the player
+      FACEIT Data API GET /players/{player_id}/history
        Input:
        player_id * string (path) - The id of the player
        game * string (query) - A game on FACEIT
@@ -132,82 +148,69 @@ async function getPastMatchesByPlayer(playerId, numMatches = 20) {
        Request URL - https://open.faceit.com/data/v4/players/20dcc7de-c82b-4d12-9bbd-b9c448b63888/history?game=csgo&offset=0&limit=20
     */
 
-    const response = await axios.get("https://open.faceit.com/data/v4/players/" + playerId + "/history?game=csgo&offset=0&limit=" + numMatches)
-    const playerWinRates = {
-        "de_cache": {"won": 0, "lost": 0},
-        "de_dust2": {"won": 0, "lost": 0},
-        "de_mirage": {"won": 0, "lost": 0},
-        "de_nuke": {"won": 0, "lost": 0},
-        "de_overpass": {"won": 0, "lost": 0},
-        "de_train": {"won": 0, "lost": 0},
-        "de_inferno": {"won": 0, "lost": 0},
-    }
+  const response = await axios.get(
+    "https://open.faceit.com/data/v4/players/" +
+      playerId +
+      "/history?game=csgo&offset=0&limit=" +
+      numMatches
+  );
+  const playerWinRates = {
+    de_cache: { won: 0, lost: 0 },
+    de_dust2: { won: 0, lost: 0 },
+    de_mirage: { won: 0, lost: 0 },
+    de_nuke: { won: 0, lost: 0 },
+    de_overpass: { won: 0, lost: 0 },
+    de_train: { won: 0, lost: 0 },
+    de_inferno: { won: 0, lost: 0 },
+  };
 
-    let matchIds = []
-    let statsPromises = []
-    response.data.items.forEach(match => {
-        matchIds.push(match.match_id)
-        statsPromises.push(parseMatchWon(match, playerId))
-    })
+  let matchIds = [];
+  let statsPromises = [];
+  response.data.items.forEach((match) => {
+    matchIds.push(match.match_id);
+    statsPromises.push(parseMatchWon(match, playerId));
+  });
 
-    const listOfResults = await Promise.all(statsPromises)
-    listOfResults.forEach(mapWL => {
-        let map = Object.keys(mapWL)[0]
-        playerWinRates[map][mapWL[map]] += 1
-    })
+  const listOfResults = await Promise.all(statsPromises);
+  listOfResults.forEach((mapWL) => {
+    let map = Object.keys(mapWL)[0];
+    playerWinRates[map][mapWL[map]] += 1;
+  });
 
-    return {"playerWinRates": playerWinRates, "matchIds": matchIds}
+  return { playerWinRates: playerWinRates, matchIds: matchIds };
 }
 
 // TODO: Make version from above where you pass in match_ids instead of player (these already collected)
 
 async function getMatchStats(matchId) {
-    /* Input: matchId, playerId
-       Output: matchInfo
+  /* Input: matchId
+     Output: matchStats
     */
-    // Map needs to be found because it's not in this original json for some reason..
-    const result = await axios.get("https://open.faceit.com/data/v4/matches/"+matchId+"/stats") //.then(response => {
-    return result.data
-    //     resolve(response.data)
-    // }).catch(error => {
-    //     rej(error)
-    //     console.log(error)
-    // })
-
+  const result = await axios.get(
+    "https://open.faceit.com/data/v4/matches/" + matchId + "/stats"
+  );
+  return result.data;
 }
 
-// function getMatchStats(matchId) {
-//     /* Input: matchId, playerId
-//        Output: matchInfo
-//     */
-//     // Map needs to be found because it's not in this original json for some reason..
-//     return new Promise((resolve, rej) => {
-//         axios.get("https://open.faceit.com/data/v4/matches/"+matchId+"/stats").then(response => {
-//             resolve(response.data)
-//         }).catch(error => {
-//             rej(error)
-//             console.log(error)
-//         })
-//     })
-
-// }
-
 function parseMatchWon(matchJson, playerId) {
-    /* Input: Match as JSON, playerId
-       Output: {map: {"win": #wins, "lose": #lost}}
+  /* Input: Match as JSON, player_id
+     Output: {map: {"win": #wins, "lose": #lost}}
     */
-    const matchId = matchJson.match_id
-    // Map needs to be found because it's not in this original json for some reason..
-    return new Promise((resolve, rej) => {
-        axios.get("https://open.faceit.com/data/v4/matches/" + matchId).then(response => {
-            const map = response.data.voting.map.pick[0]
-            const winningTeam = matchJson.results.winner // check if playerId in faction
-            const mapWon = matchJson.teams[winningTeam].players.map(player => player.player_id).includes(playerId)
-            resolve({[map]: mapWon ? "won" : "lost"})
-        }).catch(error => {
-            rej(error)
-            console.log(error)
-        })
-    })
-
+  const matchId = matchJson.match_id;
+  return new Promise((resolve, rej) => {
+    axios
+      .get("https://open.faceit.com/data/v4/matches/" + matchId)
+      .then((response) => {
+        const map = response.data.voting.map.pick[0];
+        const winningTeam = matchJson.results.winner; // check if playerId in faction
+        const mapWon = matchJson.teams[winningTeam].players
+          .map((player) => player.player_id)
+          .includes(playerId);
+        resolve({ [map]: mapWon ? "won" : "lost" });
+      })
+      .catch((error) => {
+        rej(error);
+        console.log(error);
+      });
+  });
 }
